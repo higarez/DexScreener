d2588286bf898bf0b6f1f09cc86971924875ca50
higarez <higarez0@gmail.com>
2024-10-13 08:16:16 +0300

    Update linux_komut_gonderme.py

--- linux_komut_gonderme.py
+++ linux_komut_gonderme.py
@@ -1,7 +1,7 @@
 import paramiko
 import argparse
 from datetime import datetime
-
+import time
 servers = [
     {'ip': '2.59.117.127', 'username': 'root', 'password': 'salata'},
     {'ip': '2.59.117.134', 'username': 'root', 'password': 'salata'},
@@ -21,6 +21,8 @@ def get_free_memory(ip, username, password):
         stdin, stdout, stderr = client.exec_command('free -h')
         output = stdout.read().decode()
         
+        client.close()
+        
         # Boş RAM değerini ayrıştır
         for line in output.splitlines():
             if 'Mem:' in line:
@@ -30,14 +32,11 @@ def get_free_memory(ip, username, password):
         
     except Exception as e:
         print(f"{ip} üzerinde hata oluştu: {e}")
-    finally:
-        client.close()
-    
+        
     return None
     
  
-def run_remote_command(hostname, username, password, token_address):
-    while True:
+def run_remote_command(hostname, username, password, token_address):    
         try:
             # SSH istemcisi oluştur
             client = paramiko.SSHClient()
@@ -49,49 +48,50 @@ def run_remote_command(hostname, username, password, token_address):
             
             # Komutu çalıştır
             #stdin, stdout, stderr = client.exec_command(commands)
-            client.exec_command(f"cd DexScreener && python3 DexScenTokenKontrol.py --add={token_address}")
+            stdin, stdout, stderr = client.exec_command(f"cd DexScreener && python3 DexScenTokenKontrol.py --add={token_address}")
             client.close()
             #exit()
-            break
             # Çıktıyı oku
-            #print(stdout.read().decode())
-            #print(stderr.read().decode())
-        
+            print(stdout.read().decode())
+            print(stderr.read().decode())
+            break
         except Exception as e:
             print(f"Hata: {e}")
-            continue        
+            continue
+        
         
 def main(token_address):
     max_free_memory = 0
     target_server = None
-
-    # Her sunucu için boş RAM miktarını kontrol et
-    for server in servers:
-        free_memory = get_free_memory(server['ip'], server['username'], server['password'])
-        #print(f"{server['ip']} için boş RAM: {free_memory}")
-        
-        # Boş RAM'i karşılaştır
-        if free_memory and free_memory.endswith('Gi'):  # Eğer GiB ise
-            free_value = float(free_memory[:-2])  # Son iki karakteri çıkar
-            if free_value > max_free_memory:
-                max_free_memory = free_value
-                target_server = server
-
-    # En yüksek boş RAM'e sahip sunucuya komut gönder
-    if target_server:
-        #print(f"En yüksek boş RAM'e sahip sunucu: {target_server['ip']} ({max_free_memory} GiB)")
+    while True:
         try:
-            with open("ServerCall.txt", "a") as f:
-                f.write(f"{target_server['ip']};({max_free_memory} GB);{(datetime.today().isoformat())[:-5]};{token_address}\n")
-                f.close()
-        except Exception as e:
-                print(e)
-        run_remote_command(target_server['ip'], target_server['username'], target_server['password'], token_address)
-        exit()
-        # Burada komut gönderebilirsiniz
-        # Örneğin: get_free_memory(target_server, 'username', 'password')
-        # Veya başka bir komut gönderin
-        
+            # Her sunucu için boş RAM miktarını kontrol et
+            for server in servers:
+                free_memory = get_free_memory(server['ip'], server['username'], server['password'])
+                print(f"{server['ip']} için boş RAM: {free_memory}")
+                
+                # Boş RAM'i karşılaştır
+                if free_memory and free_memory.endswith('Gi'):  # Eğer GiB ise
+                    free_value = float(free_memory[:-2])  # Son iki karakteri çıkar
+                    if free_value > max_free_memory:
+                        max_free_memory = free_value
+                        target_server = server
+
+            # En yüksek boş RAM'e sahip sunucuya komut gönder
+            if target_server:
+                print(f"En yüksek boş RAM'e sahip sunucu: {target_server['ip']} ({max_free_memory} GiB)")
+                try:
+                    with open("ServerCall.txt", "a") as f:
+                        f.write(f"{target_server['ip']};({max_free_memory} GB);{(datetime.today().isoformat())[:-5]};{token_address}\n")
+                        f.close()
+                except Exception as e:
+                        print(e)
+                run_remote_command(target_server['ip'], target_server['username'], target_server['password'], token_address)
+                break
+                exit()
+        except:
+            time.sleep(5)
+            pass
        
 
 if __name__ == "__main__":
